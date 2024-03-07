ff48ed60e9d7116e58f6d8d8a2324a5f2c5d51fa
soo pill <soopill0405@naver.com>
2024-03-07 15:15:39 +0900

    정적,동적

---
+++ codecamp/class/pages/section05/05-02-static-routing-board-moved/2/index.js
@@ -0,0 +1,3 @@
+export default function StaticRoutingMovedPage() {
+  return <div>2번 완료되었습니다.</div>;
+}
---
+++ codecamp/class/pages/section05/05-02-static-routing-board-moved/3/index.js
@@ -0,0 +1,3 @@
+export default function StaticRoutingMovedPage() {
+  return <div>3번 완료되었습니다.</div>;
+}
