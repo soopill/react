d1b2d3f3646b58189ba0039456eb796c1080a7d8
soo pill <soopill0405@naver.com>
2024-03-11 15:03:22 +0900

    훅패턴 스테이트

---
+++ codecamp/class/pages/section07/07-01-set-state/index.js
@@ -0,0 +1,4 @@
+import BoardWrite from "@/src/components/units/board/07-write/BoardWrite.cotainer";
+export default function GraphqlMutationPage() {
+  return <BoardWrite />;
+}
--- codecamp/class/src/components/units/07-write/BoardWrite.cotainer.js
+++ codecamp/class/src/components/units/07-write/BoardWrite.cotainer.js
@@ -4,6 +4,7 @@ import { 나의그래프큐엘셋팅 } from "./BoardWrite.queries";
 import BoardWriteUI from "./BoardWrite.presenter";
 
 export default function BoardWrite() {
+  const [isActive, setIsAtive] = useState(false);
   const [writer, setWriter] = useState();
   const [title, setTitle] = useState();
   const [contents, setContents] = useState();
@@ -23,21 +24,31 @@ export default function BoardWrite() {
 
     const onChangeWriter = (event) => {
       setWriter = event.target.value;
+      if (event.target.value && title && contents) {
+        setIsAtive(true);
+      }
     };
     const onChangeTitle = (event) => {
       setTitle = event.target.value;
+      if (writer && event.target.value && contents) {
+        setIsAtive(true);
+      }
     };
     const onChangeContents = (event) => {
       setContents = event.target.value;
+      if (writer && title && event.target.value) {
+        setIsAtive(true);
+      }
     };
   };
 
   return (
     <BoardWriteUI
-      aaa={onClickSubmit}
-      bbb={onChangeWriter}
-      ccc={onChangeTitle}
-      ddd={onChangeContents}
+      onClickSubmit={onClickSubmit}
+      onChangeWriter={onChangeWriter}
+      onChangeTitle={onChangeTitle}
+      onChangeContents={onChangeContents}
+      isActive={isActive}
     />
   );
 }
