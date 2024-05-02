5e4197138304273f0fd41a393ff9892609ab6932
soo pill <soopill0405@naver.com>
2024-05-02 13:40:09 +0900

    리팩토링

--- codecamp/class/pages/section16/16-03-input-refactoring/02-after.tsx
+++ codecamp/class/pages/section16/16-03-input-refactoring/02-after.tsx
@@ -13,6 +13,11 @@ const 나의그래프큐엘셋팅 = gql`
 `;
 
 export default function GraphqlMutationPage() {
+  const [inputs, setinputs] = useState({
+    writer: "",
+    title: "",
+    contents: "",
+  });
   const [writer, setWriter] = useState();
   const [title, setTitle] = useState();
   const [contents, setContents] = useState();
@@ -23,29 +28,23 @@ export default function GraphqlMutationPage() {
     const result = await 나의함수({
       variables: {
         // variables가 $임
-        writer,
-        title,
-        contents,
+        ...inputs,
       },
     });
     console.log(result);
 
-    const onChangeWriter = (event) => {
-      setWriter = event.target.value;
+    const onChangeInputs = (event) => {
+      setinputs((prev) => ({
+        ...prev,
+        [event.target.id]: event.target.value,
+      }));
     };
-    const onChangeTitle = (event) => {
-      setTitle = event.target.value;
-    };
-    const onChangeContents = (event) => {
-      setContents = event.target.value;
-    };
-  };
 
   return (
     <div>
-      작성자: <input type="text" onChange={setWriter} />
-      제목: <input type="text" onChange={setTitle} />
-      내용: <input type="text" onChange={setContents} />
+      작성자: <input type="text" id="writer" onChange={onChangeInputs} />
+      제목: <input type="text" id="title" onChange={onChangeInputs} />
+      내용: <input type="text" id="contents" onChange={onChangeInputs} />
       <button onClick={onClickSubmit}>GRAPHQL-ApI</button>;
     </div>
   );
